1facabee57f865a4b7b462e67eb83e51d718d989
Guilherme <70561321+YuutaDragon@users.noreply.github.com>
2022-01-19 20:32:01 -0300

    Assets Commits

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float runSpeed = 40f;
+    public float moveSpeed = 
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
